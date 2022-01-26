f9a5d4bf8e0e38861b81b4fd8b4c882d635bdf0c
Joyugyeong <vlxm37@naver.com>
2022-01-26 20:32:57 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ baekjoon18108/Main.java
@@ -0,0 +1,12 @@
+package baekjoon18108;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int num = s.nextInt();
+		System.out.print(num-543);
+	}
+
+}
