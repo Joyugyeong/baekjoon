88cab9b8c10d13d846a267e953ab2375820f8918
Joyugyeong <vlxm37@naver.com>
2022-02-08 13:59:15 +0900

    수 정렬하기2

--- baekjoon2751/Main.java
+++ baekjoon2751/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder(); // 붙여주는 역할
 		int N = s.nextInt();
-		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<Integer> list = new ArrayList();
 		for(int i=0;i<N;i++)
 		{
 			list.add(s.nextInt()); // 리스트에 숫자넣기 
