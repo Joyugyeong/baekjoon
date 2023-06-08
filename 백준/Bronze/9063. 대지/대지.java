6a742e8b24a74c237b6c7ca2fdc7e522b5591ea9
Joyugyeong <67578750+Joyugyeong@users.noreply.github.com>
2023-06-08 14:41:37 +0900

    [Bronze III] Title: 대지, Time: 1016 ms, Memory: 110900 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	int N = sc.nextInt();
+    	int [] x = new int[N];
+    	int [] y = new int[N];
+    	for(int i=0;i<N;i++) {
+    		x[i]= sc.nextInt();
+    		y[i]= sc.nextInt();
+    	}
+    	Arrays.sort(x);
+    	Arrays.sort(y);
+    	System.out.print((x[N-1]-x[0])*(y[N-1]-y[0]));
+    }
+}
