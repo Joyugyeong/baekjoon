3e5414da0ca994e19a3f5fc1450bbfbd5558978e
Joyugyeong <67578750+Joyugyeong@users.noreply.github.com>
2023-07-26 18:21:55 +0900

    [Silver I] Title: 카드 구매하기, Time: 152 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int arr [] = new int [N+1];
+		int d[] = new int[N+1];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <=i ; j++) {
+				d[i] = Math.max(d[i], d[i - j] + arr[j]);
+			}
+		}
+		
+		System.out.println(d[N]);
+		
+	}
+}
+
