2094a402895777f812a70617f0c823f1dc206c9d
Joyugyeong <67578750+Joyugyeong@users.noreply.github.com>
2023-08-23 15:27:04 +0900

    [Silver III] Title: N과 M (3), Time: 440 ms, Memory: 69144 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,43 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static int [] arr;
+
+	static int N;
+	static int M;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int [M];
+		
+		
+		
+		dfs(0);
+		System.out.println(sb);
+		
+    }
+    public static void dfs(int depth) {
+    	
+    	if(depth == M) {
+    		for(int val : arr) {
+    			sb.append(val).append(' ');
+    		}
+    		sb.append('\n');
+    		return;
+    	}
+    	
+    	for(int i = 1 ; i <= N ;i++) {
+    		arr[depth] = i;
+    		dfs(depth+1);
+    	}
+    	return;
+    }
+}
